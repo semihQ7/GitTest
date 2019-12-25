69e25780945a30791c488b1070d0e87abf831b06
semih <semih>
2019-12-25 23:13:07 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Semih Donmez on 25.12.2019.
+//  Copyright © 2019 Semih Donmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
